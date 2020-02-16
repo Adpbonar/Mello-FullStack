240f81f504cc5ed9fd320eb54b696eb5f9bc80ad
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2020-02-16 16:48:45 -0500

    Finished signin/signout & working on board creation

---
+++ public/assets/js/ajax-setup.js
@@ -0,0 +1,16 @@
+$.ajaxSetup({
+  headers: setHeaders()
+});
+
+function setHeaders() {
+  let jwt = localStorage.getItem('authorization');
+
+  if (jwt) {
+    return {
+      authorization: jwt
+    }
+  }
+
+  return {};
+
+}
